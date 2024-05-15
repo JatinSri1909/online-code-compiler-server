1c162be20d562dc7502a372b668727663abca7e3
Jatin Srivastava <137695680+JatinSri1909@users.noreply.github.com>
2024-05-15 17:35:01 +0530

    atlas added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const { generateFile } = require('./generateFile');
 const { addJobToQueue } = require('./jobQueue');
@@ -8,6 +11,7 @@ const Job = require('./models/Job');
 
 const app = express();
 PORT = process.env.PORT || 5000;
+MONGODB_URI = process.env.MONGODB_URI;
 
 app.use(cors());
 app.use(express.json());
@@ -56,7 +60,7 @@ app.post('/run', async(req, res) => {
     });
 
 const connectDb = async () => {
-    await mongoose.connect('mongodb://127.0.0.1:27017/online-compiler');
+    await mongoose.connect(MONGODB_URI);
     console.log(`Connected to MongoDB with ${mongoose.connection.host}`);
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
